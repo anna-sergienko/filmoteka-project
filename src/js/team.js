259149b750f4d6c9c9c3544fc226f6e267ec1f3c
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2022-01-20 00:02:33 +0200

    team-modals-fix

--- src/js/team.js
+++ src/js/team.js
@@ -1,6 +1,6 @@
 import refs from './refs.js';
 
-const { footerLink, teamModalBackdrop, teamModalCloseBtn } = refs;
+const { footerLink, teamModalBackdrop, teamModalCloseBtn, bodyLightbox } = refs;
 
 footerLink.addEventListener("click", teamModalOpen);
 teamModalCloseBtn.addEventListener("click", teamModalCloseOnBtn);
@@ -10,26 +10,27 @@ teamModalBackdrop.addEventListener("click", teamModalCloseOnBackdrop)
 // ----- открытие модалки команды по кнопке-----
 function teamModalOpen(e) {
   window.addEventListener('keydown', teamModalCloseOnEscape);
-
-  teamModalBackdrop.classList.remove("team__backdrop-is-hidden"); 
+  teamModalBackdrop.classList.remove("team__backdrop-is-hidden");
+  bodyLightbox.classList.add('lightbox__open')
 }
 
 // ----- закрытие модалки команды по кнопке-----
 function teamModalCloseOnBtn(e) {
   window.removeEventListener('keydown', teamModalCloseOnEscape);
-  teamModalBackdrop.classList.add("team__backdrop-is-hidden"); 
+  teamModalBackdrop.classList.add("team__backdrop-is-hidden");
+  bodyLightbox.classList.remove('lightbox__open')
 }
 
 // ----- закрытие модалки команды по клику вне модалки-----
 function teamModalCloseOnBackdrop(e) {
-  if(e.currentTarget === e.target) {
+  if (e.currentTarget === e.target) {
     teamModalCloseOnBtn();
   };
 }
 
- // ----- закрытие модалки команды по Escape-----
+// ----- закрытие модалки команды по Escape-----
 function teamModalCloseOnEscape(e) {
-  if(e.code === 'Escape') {
+  if (e.code === 'Escape') {
     teamModalCloseOnBtn();
   };
 }
